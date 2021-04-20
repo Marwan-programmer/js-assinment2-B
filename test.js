4a3b3be11682c5069205a305bf0af861f220c492
marwan <marwan.alsubayi@gmail.com>
2021-04-20 16:59:23 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+
+
+const uservalue =+prompt("enter a number.")|| 0;
+
+ if((uservalue % 2)=== 0 ){
+
+ alert(uservalue +" is even")
+
+ }else{
+
+    alert(uservalue +" is odd")
+
+ };
+
+
+
+
+// it is possible to use Conditional (ternary) operator instead of if Condition
+
+//    result=(uservalue %2===0) ? (uservalue  + " is even") : (uservalue  + " is odd")
+//    alert(result);
